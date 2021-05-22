c080d951e57fd78f5bdfb1a554433ce2d72650a9
MPS Apps <mpsappshelp@gmail.com>
2021-05-22 15:20:05 +0530

    file

--- action.php
+++ action.php
@@ -5,16 +5,16 @@
 		exit;
 	}
 
-	if (empty($_POST["fullname"]) || empty($_POST["feedback"])) {
+	if (empty($_POST["fullname"]) || empty($_POST["feedback"]) || empty($_POST["email"])) {
 		echo "Please fill the form";
 		exit;
 	}
 
 	$name = $_POST["fullname"];
+	$email = $_POST["email"]
 	$feedback = $_POST["feedback"];
 
-	mail('mpyt7410@gmail.com', 'New form submission', 'Name : $name, Feedback : $feedback');
-
-	header('Location : index.html');
+	mail('mpyt7410@gmail.com', 'New form submission', 'Name : $name, Email : $email, Feedback : $feedback');
+	
 
 ?>
